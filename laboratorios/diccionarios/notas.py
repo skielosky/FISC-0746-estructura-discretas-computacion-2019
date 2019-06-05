f32b50b4180b683b41f1c0264413f447cd641be0
Ezequiel Aguilar <ezequiel.aguilarg@gmail.com>
2019-06-05 15:25:39 -0500

    Add files via upload

---
+++ laboratorios/diccionarios/notas.py
@@ -0,0 +1,17 @@
+grade_book = {
+    'Susana': [92, 85, 100],
+    'Eduardo': [83, 95, 79],
+    'Ariel': [91, 89, 82],
+    'Pedro': [97, 91, 92]
+}
+
+all_grades_total = 0
+all_grades_count = 0
+
+for name, grades in grade_book.items():
+    total = sum(grades)
+    print(f'Average for {name} is {total / len(grades):.2f}')
+    all_grades_total += total
+    all_grades_count += len(grades)
+
+print(f"Class's average is: {all_grades_total / all_grades_count:.2f}")
